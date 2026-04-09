b585fccefc8c417f225ff75ed7a2c68cd1b55c3b
batinkaska <batinyigitkaska@gmail.com>
2026-04-09 16:38:20 +0300

    vite.config

---  vite.config.js
+++  vite.config.js
@@ -1,12 +1,3 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  optimizeDeps: {
-    include: ['firebase/app', 'firebase/database', 'firebase/auth']
-  },
-  build: {
-    rollupOptions: {
-      external: []
-    }
-  }
-})
+export default defineConfig({})
